893edb70bfc8a1475e171dadf843a3a8059d7982
laraturrion <laturrions@gmail.com>
2024-01-04 13:45:57 +0100

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,21 @@
 import "./header.css";
 import logotipo from "../../assets/logo_reactflix.svg";
-import { Enlace } from "./enlace";
-
 
 export const Header = () => {
   return (
-    <nav className="navegador">
-      <img src={logotipo} alt="logotipo" className="logo" />
-      <Enlace />
+    <nav className="navHeader">
+      <div className="Navegador">
+        <img src={logotipo} alt="logotipo" className="logo" />
+        <a href="">Inicio</a>
+        <a href="">Series Tv</a>
+        <a href="">Peliculas</a>
+        <a href="">Infantil</a>
+        <a href="">Mi lista</a>
+      </div>
+      <div className="perfilBuscador">
+        <button>Lupa</button>
+        <img src="" alt="Foto de perfil" className="fotoPerfil" />
+      </div>
     </nav>
   );
 };
