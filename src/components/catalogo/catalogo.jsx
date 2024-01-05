4e76f1df4ad63c4e68727c49cda0081ef3114e30
Iciar <rgiciar@gmail.com>
2024-01-05 13:48:49 +0100

    subido catalogo

--- src/components/catalogo/catalogo.jsx
+++ src/components/catalogo/catalogo.jsx
@@ -1,11 +1,18 @@
 import "./catalogo.css";
 
 export const Catalogo = () => {
+  const peliculas = datosPeliculas.results;
+
   return (
-    <>
-
-
-    
-    </>
-  )
-}
+      <section>
+          {peliculas.map((pelicula, index) => (
+              <article key={index} className="pelicula">
+                  <img
+                      src={`https://image.tmdb.org/t/p/original/${pelicula.poster_path}`}
+                      alt={`Portada de la película ${index + 1}`}
+                  />
+              </article>
+      ))}
+      </section>
+  );
+};
