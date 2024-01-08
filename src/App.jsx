c1674120f25c61c245784b02b0c0a79cb0e4d92e
laraturrion <laturrions@gmail.com>
2024-01-08 12:30:19 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Header } from "./components/header/Header";
 import { Catalogo } from "./components/catalogo/Catalogo";
+import { Footer } from "./components/footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Catalogo />
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="Footer">
+      <a href="">Preguntas Frecuentes</a>
+      <a href="">Centro de Ayuda</a>
+      <a href="">Tarifas</a>
+      <a href="">Contacto</a>
+    </div>
+  );
+};
